7208479ec3db3c959d26dd08909b36943de145d2
Xia <xiadezhi2001@gmail.com>
2025-02-08 18:53:43 +0800

    feat: print connected subgraph and support analyzing sol transfer

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 from solders.pubkey import Pubkey
 import networkx as nx
 import matplotlib.pyplot as plt
-from datetime import datetime
+
 
 
 def is_token_transfer(tx_data) -> bool:
@@ -19,8 +19,21 @@ def is_token_transfer(tx_data) -> bool:
     except (KeyError, TypeError):
         return False
 
+def is_sol_transfer(tx_data) -> int:
+    try:
+        # 获取 logMessages
+        log_messages = tx_data.value.transaction.meta.log_messages
 
-def get_transfer_receiver(address:str, tx_data, target_mint: str):
+        # 计算目标消息出现的次数
+        count = log_messages.count('Program 11111111111111111111111111111111 invoke [1]')
+        return count
+
+    except (KeyError, TypeError):
+        return 0
+
+
+
+def get_token_receiver(address:str, tx_data, target_mint: str):
     try:
         # 获取 pre 和 post token balances
         post_token_balances = tx_data.value.transaction.meta.post_token_balances
@@ -32,25 +45,39 @@ def get_transfer_receiver(address:str, tx_data, target_mint: str):
         for balance in post_token_balances:
             if balance.mint == mintPub and balance.owner != addressPub:
                 receiver.append(str(balance.owner))
-
         return receiver
 
     except (KeyError, TypeError):
-        # 如果获取数据失败，返回 False
-        return False
+        return []
 
+def get_sol_receiver(address:str,tx_data,receiver_counts:int):
+    try:
+        account_keys = tx_data.value.transaction.transaction.message.account_keys
+        index = account_keys.index(address)
+        end_index = index + 1 + receiver_counts
+        return account_keys[index + 1:end_index]
+    except (KeyError, TypeError,ValueError):
+        return []
 
 class SolanaTransferAnalyzer:
-    def __init__(self, rpc_url="rpc_url"):
+    def __init__(self, rpc_url="https://mainnet.helius-rpc.com/?api-key=28559d54-a5d0-483f-8154-d5c29989db7f"):
         self.client = Client(rpc_url)
         self.graph = nx.Graph()
 
     def construct_graph(self,addresses):
         for address in addresses:
             self.graph.add_node(address)
+
     def add_edge(self,node,neighbors):
         for neighbor in neighbors:
             self.graph.add_edge(node,neighbor)
+
+    def print_all_connected_subgraph(self, max_node_count):
+        for c in nx.connected_components(self.graph):
+            nodeSet = self.graph.subgraph(c).nodes
+            if len(nodeSet) <= max_node_count:
+                print(nodeSet,end="\n")
+
     def get_graph_neighbour(self, address: str, mint_address: str,time_interval):
         current =  int(time.time())
         neighbors = []
@@ -75,10 +102,15 @@ class SolanaTransferAnalyzer:
                             encoding="jsonParsed",
                             max_supported_transaction_version=0
                         )
-
-                        if is_token_transfer(tx_data):
-                            receivers = get_transfer_receiver(address, tx_data, mint_address)
-                            neighbors.extend(receivers)
+                        if mint_address == 'sol':
+                            count = is_sol_transfer(tx_data)
+                            if count > 0:
+                                receivers = get_sol_receiver(address, tx_data, count)
+                                neighbors.extend(receivers)
+                        else:
+                            if is_token_transfer(tx_data):
+                                receivers = get_token_receiver(address, tx_data, mint_address)
+                                neighbors.extend(receivers)
 
                         # 添加延时以避免请求过快
                         time.sleep(0.1)
@@ -93,6 +125,10 @@ class SolanaTransferAnalyzer:
 def main():
     # 初始化
     analyzer = SolanaTransferAnalyzer()
+    #如果是sol
+    #token_mint = "sol"
+
+    #ava为例
     token_mint = "DKu9kykSfbN5LBfFXtNNDPaX35o4Fv6vJ9FKk7pZpump"
     wallet1 = "8SJR9CNrANJvgWVND9ZPNHK6va6VCC3p7NBSx8c3hotM"
     wallet2 = "w6AovkEzgdQYt6yJDN7atHMkRQv877FpXWvRKouv6Gc"
@@ -109,14 +145,8 @@ def main():
         neighbors = analyzer.get_graph_neighbour(wallet, token_mint,seven_days_in_seconds)
         analyzer.add_edge(wallet,neighbors)
 
-    # 绘制图
-    pos = nx.spring_layout(analyzer.graph)  # 计算节点位置
-    nx.draw(analyzer.graph, pos, with_labels=True, node_size=500, node_color='skyblue', font_size=20, font_color='black',
-            edge_color='gray')
-
-    # 显示图
-    plt.axis('off')  # 不显示坐标轴
-    plt.savefig('output.png')
+    #打印结果：只打印节点数量小于3的集群
+    analyzer.print_all_connected_subgraph(3)
 
 
 if __name__ == "__main__":
